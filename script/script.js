7a06a90541edfa095c430b444a32fa6124e81cc8
Théodore Imperato <theodore048@gmail.com>
2022-12-31 01:39:42 +0100

    Création d'un système qui écrit le nom Option SISR en typing grâce au JavaScript Task #45 - Création d'un système qui écrit le nom de la famille automatiquement

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["BTS SIO Option SISR"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["BTS SIO option SISR"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
